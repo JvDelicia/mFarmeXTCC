07ec009accf7e818eb584e56474b7b104b3eba38
siolinm <36745419+siolinm@users.noreply.github.com>
2018-11-27 17:39:56 -0200

    Consulta Gênero

--- app/src/main/java/com/example/a3aetim/Myndie/SearchableActivity.java
+++ app/src/main/java/com/example/a3aetim/Myndie/SearchableActivity.java
@@ -34,6 +34,7 @@ import com.example.a3aetim.Myndie.Connection.AppConfig;
 import com.example.a3aetim.Myndie.Connection.AppController;
 import com.example.a3aetim.Myndie.helper.DatabaseHelper;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -47,7 +48,7 @@ import static com.example.a3aetim.Myndie.Connection.AppController.TAG;
 public class SearchableActivity extends AppCompatActivity {
     private DatabaseHelper helper;
     private SQLiteDatabase db;
-    private ArrayList<Application> app;
+    public ArrayList<Application> app;
     private RecyclerView mRecyclerView;
     private ApplicationAdapter mRVAdapter;
     private RecyclerView.LayoutManager mRVLManager;
@@ -63,7 +64,8 @@ public class SearchableActivity extends AppCompatActivity {
         toolbar.setTitle("");
         mRecyclerView = findViewById(R.id.recyclerSearchActivity);
         mRecyclerView.setHasFixedSize(true);
-        setmRecyclerView();
+        searchHandler(getIntent());
+
     }
 
     @Override
@@ -75,12 +77,10 @@ public class SearchableActivity extends AppCompatActivity {
     private void load(){
         helper = new DatabaseHelper(this);
         db = helper.getReadableDatabase();
-        app = new ArrayList<>();
         mRVLManager = new LinearLayoutManager(this);
     }
     private void setmRecyclerView(){
         load();
-        searchHandler(getIntent());
         mRVAdapter = new ApplicationAdapter(app);
         mRecyclerView.setLayoutManager(mRVLManager);
             mRecyclerView.setAdapter(mRVAdapter);
@@ -123,7 +123,8 @@ public class SearchableActivity extends AppCompatActivity {
                 if(intent.getSerializableExtra("Genre") != null){
                     Genre g = (Genre) intent.getSerializableExtra("Genre");
                     toolbar.setTitle(g.getName());
-                    //getAppsByGenre(g.get_IdGenre());
+                    app = new ArrayList<>();
+                    getAppsByGenre(g.get_IdGenre());
                 }
                 else{
                     String query = intent.getStringExtra(SearchManager.QUERY);
@@ -133,10 +134,10 @@ public class SearchableActivity extends AppCompatActivity {
         }
     }
 
-   /* private void getAppsByGenre(int idGenre) {
+    private void getAppsByGenre(final int idGenre) {
 
         String tag_string_req = "req_login";
-
+        final ArrayList arrayList = new ArrayList();
         StringRequest strReq = new StringRequest(Request.Method.POST,
                 AppConfig.URL_ConsultaAppGeneros, new Response.Listener<String>() {
 
@@ -145,24 +146,29 @@ public class SearchableActivity extends AppCompatActivity {
                 Log.d(TAG, "Login Response: " + response.toString());
 
                 try {
-                    JSONObject jObj = new JSONObject(response);
-                    boolean error = jObj.getBoolean("error");
+                    JSONArray jObj = new JSONArray(response);
 
-                    // Check for error node in json
-                    if (!error) {
-                        // Now store the user in SQLite
-                        String uid = jObj.getString("Id");
-
-                        JSONObject user = jObj.getJSONObject("User");
-                        User usuario = new User(user);
-                        logar(usuario);
-
-                    } else {
-                        // Error in login. Get the error message
-                        String errorMsg = jObj.getString("error_msg");
-                        Toast.makeText(getApplicationContext(),
-                                errorMsg, Toast.LENGTH_LONG).show();
+                    for (int i = 0; i < jObj.length(); i++) {
+                        JSONObject jsonObjectApp = jObj.getJSONObject(i);
+                        int idapp = Integer.parseInt(jsonObjectApp.getString("Id"));
+                        String title = jsonObjectApp.getString("Name");
+                        String desc = jsonObjectApp.getString("Desc");
+                        String version = jsonObjectApp.getString("Version");
+                        double price = Double.parseDouble(jsonObjectApp.getString("Price"));
+                        String publisher = jsonObjectApp.getString("PublisherName");
+                        String releasedate = jsonObjectApp.getString("ReleaseDate");
+                        String imageURL = jsonObjectApp.getString("ImageUrl");
+                        imageURL = "https://myndie.azurewebsites.net/"+imageURL;
+                        int devId = jsonObjectApp.getInt("DeveloperId");
+                        int typeAppId = jsonObjectApp.getInt("TypeAppId");
+                        int pegiId = jsonObjectApp.getInt("PegiId");
+                        Application objetoApp = new Application(idapp, title, price, version, desc, publisher, releasedate,imageURL,devId,typeAppId,pegiId);
+                        //Toast.makeText(SearchableActivity.this, objetoApp.getTitle(), Toast.LENGTH_SHORT).show();
+                        arrayList.add(objetoApp);
                     }
+                    app.addAll(arrayList);
+                    setmRecyclerView();
+
                 } catch (JSONException e) {
                     // JSON error
                     e.printStackTrace();
@@ -184,8 +190,7 @@ public class SearchableActivity extends AppCompatActivity {
             protected Map<String, String> getParams() {
                 // Posting parameters to login url
                 Map<String, String> params = new HashMap<String, String>();
-                params.put("idapp", email);
-                params.put("password", password);
+                params.put("idGenre", Integer.toString(idGenre));
 
                 return params;
             }
@@ -194,26 +199,7 @@ public class SearchableActivity extends AppCompatActivity {
 
         // Adding request to request queue
         AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
-        *//*Cursor cursorapp = db.rawQuery("SELECT _IdApp, NameApp, PriceApp,VersionApp, PublisherNameApp, ReleaseDateApp, DescApp from Application INNER JOIN ApplicationGenre on Application._IdApp = ApplicationGenre._IdApp_FK WHERE Application._IdApp = ApplicationGenre._IdApp_FK AND _IdGen_FK ="+idGenre, null);
-        int idapp;
-        String nameapp,version, publisher, desc;
-        double preco;
-        String releasedate;
-        cursorapp.moveToFirst();
-        for (int j = 0; j < cursorapp.getCount(); j++) {
-            idapp = cursorapp.getInt(0);
-            nameapp = cursorapp.getString(1);
-            preco = cursorapp.getDouble(2);
-            version = cursorapp.getString(3);
-            publisher = cursorapp.getString(4);
-            releasedate = cursorapp.getString(5);
-            desc = cursorapp.getString(6);
-            app.add(new Application(idapp,nameapp,preco,version,desc,publisher,releasedate));
-            cursorapp.moveToNext();
-        }
-        cursorapp.close();*//*
     }
-*/
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.searchablemenu,menu);
